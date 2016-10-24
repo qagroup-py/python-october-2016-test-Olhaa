caddd25f6359258aa89988770722f50d29e6b8a8
Olha <olha.alferova@gmail.com>
2016-10-24 23:46:01 +0300

    complete OOP_food

--- OOP_food.py
+++ OOP_food.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 """
 Implement class structure/hierarchy for following classes:
 
@@ -18,3 +19,42 @@ Red globe (grapes)  (виноград "Ред Глоб")
 Victoria (grapes)  (виногдар "Вікторія")
 Wild strawberry  (суниця)
 """
+
+class Food:
+    def __init__(self, name):
+        self.name = name
+    
+class Berries(Food):
+    pass
+
+Blueberry = Berries 
+Domestic_strawberry = Berries
+Wild_strawberry = Berries
+
+class Fruits(Food):
+    pass
+
+Concorde_Pear = Fruits
+Golden_delicious_apple = Fruits
+Granny_Smith_apple = Fruits
+Red_globe_grapes = Fruits
+Victoria_grapes = Fruits
+
+class Greens(Food):
+    pass
+
+Basil = Greens
+Dill = Greens 
+
+class Vegetables(Food):
+    pass
+
+
+Beetroor = Vegetables
+Cabbage = Vegetables
+Carrot = Vegetables 
+Onion = Vegetables
+Potato = Vegetables
+
+
+
