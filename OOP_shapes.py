ca376e33a83a7d4cdd80828919b633260938c2f3
Olha <olha.alferova@gmail.com>
2016-10-25 00:00:56 +0300

    complete OOP_shapes

--- OOP_shapes.py
+++ OOP_shapes.py
@@ -18,3 +18,34 @@ Square (квадрат)
 Tetrahedron (тетраедр)
 Triangle (трикутник)
 """
+
+class Shapes:
+    def __init__(self, name):
+        self.name = name
+
+class One_dimensional (Shapes):
+	pass
+
+Point = One_dimensional
+Line = One_dimensional
+
+class Two_dimensional (Shapes):
+	pass
+
+Circle = Two_dimensional
+Ellipse = Two_dimensional
+Hexagon = Two_dimensional
+Rectangle = Two_dimensional
+Round = Two_dimensional
+Square = Two_dimensional
+Triangle = Two_dimensional
+
+class Three_dimensional (Shapes):
+    pass
+
+Cone = Three_dimensional
+Cube = Three_dimensional
+Pyramid = Three_dimensional
+Sphere = Three_dimensional
+Tetrahedron = Three_dimensional
+Ball = Three_dimensional
